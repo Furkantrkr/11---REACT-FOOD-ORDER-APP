68d92f24c15e580123cd395c63975525617088c7
Furkan TÜRKER <furkantrkr13@gmail.com>
2023-11-18 22:39:07 +0300

    Edit CartItem component

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,7 +1,21 @@
+import { useContext } from 'react';
+import styles from './CartItem.module.css';
+import { currencyFormatter } from '../../util/formatting';
+import CartContext from '../../store/CartContext';
+
 export default function CartItem({ item }) {
+  const cartCtx = useContext(CartContext);
+
   return (
-    <li>
-      {item.name} - {item.quantity}
+    <li className={styles['cart-item']}>
+      <p>
+        {item.name} - {item.quantity} x {currencyFormatter.format(item.price)}
+      </p>
+      <p className={styles['cart-item-actions']}>
+        <button onClick={() => cartCtx.removeItem(item.id)}>-</button>
+        <span>{item.quantity}</span>
+        <button onClick={() => cartCtx.addItem(item)}>+</button>
+      </p>
     </li>
   );
 }
